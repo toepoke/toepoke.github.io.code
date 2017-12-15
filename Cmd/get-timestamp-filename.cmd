71845f7e655b98a745496035e63356e99df83059
toepoke <github@junge.co.uk>
2017-12-15 09:13:25 +0000

    fix: Get timestamp command wasn't taking into account that before 10am the TIME environment variable gives a leading space, not a leading zero.

--- Cmd/get-timestamp-filename.cmd
+++ Cmd/get-timestamp-filename.cmd
@@ -1,23 +1,30 @@
+@echo off
 REM
 REM Windows: Converts the current DATE and TIME into a filename friendly string
 REM
-@echo off
 
 echo.
 echo Based on UK Date Format, given:
 echo.
-echo Current Date	= 04/12/2017
-echo Current Time	= 11:00:23.35
-echo TimeStamp 		= 20171204_110023
+echo Current Date  =  04/12/2017
+echo Current Time  =  11:00:23.35
+echo.
+echo TimeStamp would be:
+echo TimeStamp     =  20171204_110023
 echo.
 echo Basically %%DATE:~6,4%% is a substring operation which is zero index, so %%DATE:~6,4%% gets the YEAR portion
-set TimeStamp=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+
+set DATE_PART=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
+set TIME_PART=%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+
+REM Time may have a leading space in the morning ( < 10am) - replace the space with a zero
+set TIME_PART=%TIME_PART: =0%
+
+set TimeStamp=%DATE_PART%_%TIME_PART%
 
 echo Example:
 echo ----------------
-echo Current Date: %DATE%
-echo Current Time: %TIME%
-echo %TimeStamp%
+echo Current Date  =  %DATE%
+echo Current Time  =  %TIME%
+echo TimeStamp     =  %TimeStamp%
 echo.
-echo.
-
