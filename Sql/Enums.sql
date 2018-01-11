d234f18ce21a786ddebee359a5428d5c759407de
toepoke <github@junge.co.uk>
2018-01-11 11:32:54 +0000

    SQL stuff illustrating possible approach to enumerations.

---
+++ Sql/Enums.sql
@@ -0,0 +1,33 @@
+
+-- Not saying this is the best way of doing it.
+-- I'd argue if you're thinking this is useful, you should probably be using a lookup table instead :-)
+-- BUT; It is an option.
+
+-- Thinking further this technique may be useful for stored procs where the enum is used across the system
+-- and/or where the value of the enumerations aren't saved (i.e. you can alter the index without impact on the db)
+
+create view Enumerations AS
+	select Enum, Value, ShortDesc
+	from 
+	(
+		--     Enum            Value    ShortDesc
+    select 'Animals' Enum, 1 Value, 'Cat'  ShortDesc union all
+    select 'Animals'     , 2      , 'Dog'            union all
+    select 'Animals'     , 3      , 'Bird' 
+
+		union all
+
+		--     Enum        Value   ShortDesc
+    select 'Vehicles', 1,      'Car'        union all
+    select 'Vehicles', 2,      'Bike'       union all
+    select 'Vehicles', 3,      'Lorry' 
+
+	) animals
+go
+
+select * from Enumerations
+go
+
+drop view Enumerations
+go
+
