a9c8f24f979912fd7a280916db3781457e6dbe94
toepoke <github@junge.co.uk>
2018-02-26 10:44:46 +0000

    sql: Illustration of how to find duplicate fields in a query.

---
+++ Sql/find-duplicate-data.sql
@@ -0,0 +1,35 @@
+﻿--
+-- DbServer:  n/a - general purpose script
+-- DbName:    n/a - general purpose script
+--
+-- Explanation:
+--    An approach for finding duplicate items in a query.
+--    Whilst one would ordinarily cheat with a "DISTINCT", sometimes you want to know what the duplicates are
+-- 
+-- References:
+--  - None
+
+select 
+	*
+into #shoppingList
+from
+(
+	--     Id      BasketItem
+	select 1 Id,   'Milk'  BasketItem   union all
+	select 2,      'Beans'              union all
+	select 3,      'Beans'              union all
+	select 4,      'Bread'
+) a
+
+select * from #shoppingList
+
+select
+	BasketItem, count(BasketItem) TimesDuplicated
+from
+	#shoppingList
+group by
+	BasketItem
+having
+	(count(BasketItem) > 1)
+
+drop table #shoppingList
