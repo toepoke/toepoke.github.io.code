5b31e38e9d7246328503659759220fb3a6e528ed
toepoke <github@junge.co.uk>
2018-02-26 10:43:57 +0000

    sql: Illustration of how to use Enum views.

--- Sql/Enums.sql
+++ Sql/Enums.sql
@@ -1,10 +1,19 @@
-
--- Not saying this is the best way of doing it.
--- I'd argue if you're thinking this is useful, you should probably be using a lookup table instead :-)
--- BUT; It is an option.
-
--- Thinking further this technique may be useful for stored procs where the enum is used across the system
--- and/or where the value of the enumerations aren't saved (i.e. you can alter the index without impact on the db)
+--
+-- DbServer:  n/a - general purpose script
+-- DbName:    n/a - general purpose script
+--
+-- Explanation:
+--    A method of creating enums in SQL server without look-ups tables.  Not saying this is the best way of doing it!
+--
+--    I'd argue if you're thinking this is useful, you should probably be using a lookup table instead :-)
+--    BUT; It is an option.
+--
+--    Thinking further this technique may be useful for stored procs where the enum is used across the system
+--    and/or where the value of the enumerations aren't saved (i.e. you can alter the index without impact on the db)
+--    (though I'd still be tempted to say it's a lookup table)
+-- 
+-- References:
+--  - None
 
 create view Enumerations AS
 	select Enum, Value, ShortDesc
