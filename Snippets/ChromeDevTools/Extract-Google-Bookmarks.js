e005c61e78fcaa48d2e66051d3b582fe46d582c9
toepoke <bitbucket@junge.f9.co.uk>
2018-05-13 07:45:54 +0100

    refactor: Abstracted out markdown report generation.

--- Snippets/ChromeDevTools/Extract-Google-Bookmarks.js
+++ Snippets/ChromeDevTools/Extract-Google-Bookmarks.js
@@ -3,12 +3,6 @@
 /// as a set of markdown ready for pasting.
 /// 
 
-var search = document.getElementById("search");
-var hits = [];
-
-// Find all hyperlinks to start with
-var links = search.querySelectorAll("a");
-
 function tidyTargetLink(href) {
 	if (!href) {
 		return "";
@@ -54,48 +48,68 @@ function escapeMarkdownString(md) {
 	return md.replace("|", "&#124;");
 }
 
-// Loop over all links and identify which links are actually bookmark links!
-for (var link of links) {
-	var id = link.getAttribute("id");
-	var href = link.getAttribute("href");
-	var info = "";
-
-	if (id && id.indexOf("bkmk_href_") === 0 && href && href.length > 0) {
-		var hit = {
-			text: link.text,
-			href: link.href,
-			info: "",
-			link: link
-		};
-
-		hit.href = tidyTargetLink(hit.href);
-
-		// we may have addition notes to the link we want including
-		// link.parentElement.parentElement.parentElement.querySelectorAll("td span#bkmk_info_0")[0].innerText
-		var infoId = id.replace("_href_", "_info_");
-		var infoEles = link.parentElement.parentElement.parentElement.querySelectorAll("td span#" + infoId);
-		if (infoEles && infoEles.length > 0) {
-			hit.info = extractBookmarkComment(infoEles[0]);
-		}
-
-		hit.text = escapeMarkdownString(hit.text);
-		hit.info = escapeMarkdownString(hit.info);
-		hit.href = escapeMarkdownString(hit.href);
-
-		hits.push(hit);
-	} // if bookmark link
-
+function markdownify(hits) {
 	var markdown = "";
+
 	for (var hit of hits) {
-// copy all links into markdown format
-		markdown += `* [${hit.text}](${hit.href})`;
-// add notes if there are any
-		if (hit.info && hit.info !== "") {
-			markdown += `* - ${hit.info}*`;
+		var text = escapeMarkdownString(hit.text);
+		var href = escapeMarkdownString(hit.href);
+		var info = escapeMarkdownString(hit.info);
+
+	// copy all links into markdown format
+		markdown += `* [${text}](${href})`;
+	// add notes if there are any
+		if (info && info !== "") {
+			markdown += `* - ${info}*`;
 		}
 		markdown += "\n";
 	}
-} // foreach (link)
+
+	return markdown;
+}
+
+function main() {
+	var search = document.getElementById("search");
+	var hits = [];
+
+	// Find all hyperlinks to start with
+	var links = search.querySelectorAll("a");
+
+
+	// Loop over all links and identify which links are actually bookmark links!
+	for (var link of links) {
+		var id = link.getAttribute("id");
+		var href = link.getAttribute("href");
+		var info = "";
+
+		if (id && id.indexOf("bkmk_href_") === 0 && href && href.length > 0) {
+			var hit = {
+				text: link.text,
+				href: link.href,
+				info: "",
+				link: link
+			};
+
+			hit.href = tidyTargetLink(hit.href);
+
+			// we may have addition notes to the link we want including
+			// link.parentElement.parentElement.parentElement.querySelectorAll("td span#bkmk_info_0")[0].innerText
+			var infoId = id.replace("_href_", "_info_");
+			var infoEles = link.parentElement.parentElement.parentElement.querySelectorAll("td span#" + infoId);
+			if (infoEles && infoEles.length > 0) {
+				hit.info = extractBookmarkComment(infoEles[0]);
+			}
+
+			hits.push(hit);
+		} // if bookmark link
+
+	} // foreach (link)
+
+	return hits;
+} // main
+
+var hits = main();
+var markdown = markdownify(hits);
 
 // output results
 console.clear()
