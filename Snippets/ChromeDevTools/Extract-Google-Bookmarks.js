95d73653a088088b5d364c46e5c811a0ff717009
toepoke <bitbucket@junge.f9.co.uk>
2018-05-13 06:35:24 +0100

    feat: Added any comments against the link as a sub-comment in the resulting markdown.

--- Snippets/ChromeDevTools/Extract-Google-Bookmarks.js
+++ Snippets/ChromeDevTools/Extract-Google-Bookmarks.js
@@ -13,11 +13,13 @@ var links = search.querySelectorAll("a");
 for (var link of links) {
 	var id = link.getAttribute("id");
 	var href = link.getAttribute("href");
+	var info = "";
 
 	if (id && id.indexOf("bkmk_href_") === 0 && href && href.length > 0) {
 		var hit = {
 			text: link.text,
 			href: link.href,
+			info: "",
 			link: link
 		};
 
@@ -30,13 +32,40 @@ for (var link of links) {
 			hit.href = hit.href.substring(0, pos);
 		}
 
+		// we may have addition notes to the link we want including
+		// link.parentElement.parentElement.parentElement.querySelectorAll("td span#bkmk_info_0")[0].innerText
+		var infoId = id.replace("_href_", "_info_");
+		var infoEles = link.parentElement.parentElement.parentElement.querySelectorAll("td span#" + infoId);
+		if (infoEles && infoEles.length > 0) {
+			var infoEle = infoEles[0];
+			// GB adds a summary of the tags before out comment, so skip that part out
+			hit.info = infoEle.textContent;
+			// GB also adds some preamble, so we'll remove that too
+			var preamblePos = hit.info.indexOf(" - ");
+			if (preamblePos > 0) {
+				hit.info = hit.info.substring(preamblePos + " - ".length);
+			} else {
+				// GB will just include the tags, which we aren't interested in
+				hit.info = "";
+			}
+		}
+
+		// and finally, if the info has a | in it, markdown will see this as a table cell declaration
+		// ... so escape that out
+		hit.text = hit.text.replace("|", "&#124;");
+
 		hits.push(hit);
 	} // if bookmark link
 
 	var markdown = "";
 	for (var hit of hits) {
 // copy all links into markdown format
-		markdown += `* [${hit.text}](${hit.href})\n`;
+		markdown += `* [${hit.text}](${hit.href})`;
+// add notes if there are any
+		if (hit.info && hit.info !== "") {
+			markdown += `* - ${hit.info}*`;
+		}
+		markdown += "\n";
 	}
 } // foreach (link)
 
@@ -44,6 +73,6 @@ for (var link of links) {
 console.clear()
 copy(markdown);
 console.info(markdown);
-console.warn(`${hits.length} markdown links have been copied to the clipboard.`);
+console.warn(hits, `\n${hits.length} markdown links have been copied to the clipboard.`);
 console.info("Once you're done, remember to scroll to the bottom");
 console.info("'Manage Labels' to delete the '11' label so we're ready for next time!");
