1ffa2841e59df0bbb2de536fb79ada1ef7e8b128
toepoke <github@junge.co.uk>
2018-06-19 16:11:43 +0100

    feat: Added illustration of handling events.

---
+++ c#/Unit.Tests/EventHandling/EventHandling.Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit.Tests.EventHandling_Tests {
+	
+	[TestClass]
+	public class PubSub_Tests {
+
+		[TestMethod]
+		public void When_Event_Is_Raised_By_Publisher_It_Is_Caught_By_The_Listener() {
+			// Arrange
+			int attempts = 0, maxAttempts = 1;
+			PubSub.Publisher publisher = new PubSub.Publisher();
+			PubSub.Listener listener = new PubSub.Listener();
+			
+			// Act
+			listener.Subscribe(publisher);
+
+			// Emulate a process firing an event
+			publisher.DoWork();
+			while (!listener.EventWasRaised && attempts < maxAttempts) {
+				System.Threading.Thread.Sleep(1000);
+				attempts++;
+			}
+
+			// Assert
+			Assert.IsTrue(listener.EventWasRaised);
+		}
+	}
+
+}
---
+++ c#/Unit.Tests/EventHandling/PubSub.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// See https://codereview.stackexchange.com/questions/119415/raise-an-event-sample-1
+namespace Unit.Tests.EventHandling_Tests.PubSub {
+
+	public class RaiseArgs : EventArgs {
+		public RaiseArgs(string message) {
+			this.Message = message;
+		}
+		public string Message = "";
+	}
+
+	public class Publisher {
+		public delegate void EventHandler(object sender, RaiseArgs e);
+
+		public event EventHandler Event;
+
+		public void RaiseEvent(RaiseArgs e) {
+			var handler = Event;
+			if (handler != null) {
+				handler(this, e);
+			}
+		}
+
+		public void DoWork() {
+			RaiseArgs args = new RaiseArgs("an event was raised");
+			this.RaiseEvent(args);
+		}
+	}
+
+	public class Listener {
+		public bool EventWasRaised { get; set; } = false;
+
+		public void Subscribe(Publisher publisher) {
+			publisher.Event += HeardEvent;
+		}
+
+		public void HeardEvent(object sender, RaiseArgs e) {
+			Console.Write(DateTime.Now.ToString());
+			Console.Write(" :: ");
+			Console.WriteLine(e.Message);
+			this.EventWasRaised = true;
+		}
+	}
+
+}
