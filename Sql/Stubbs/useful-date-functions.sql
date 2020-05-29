8bba85b7a7e2a10ca6f07208a27815995862205b
fj <956482+toepoke@users.noreply.github.com>
2020-05-29 13:25:37 +0100

    dev: Added all examples into a single query
    Easier to read the results.

--- Sql/Stubbs/useful-date-functions.sql
+++ Sql/Stubbs/useful-date-functions.sql
@@ -2,38 +2,39 @@
 -- Website no longer available
 
 -- First Day of Month
-select DATEADD(mm, DATEDIFF(mm,0,getdate()), 0) 'First Day of Month'
+select 'First Day of Month' Description, DATEADD(mm, DATEDIFF(mm,0,getdate()), 0) union all
  
 -- First Day of Last Month
-select DATEADD(mm, DATEDIFF(mm,0,DATEADD(mm,-1,getdate())), 0) 'First Day of Last Month'
+select 'First Day of Last Month' Description, DATEADD(mm, DATEDIFF(mm,0,DATEADD(mm,-1,getdate())), 0) union all
  
 -- Monday of the Current Week
-select DATEADD(wk, DATEDIFF(wk,0,getdate()), 0) 'Monday of the Current Week'
- 
--- Sunday of the Current Week
-set DATEFIRST 1
-select DATEADD(dd, 1 - DATEPART(dw, getdate()), getdate()) 'Sunday of the Current Week'
+select 'Monday of the Current Week' Description, DATEADD(wk, DATEDIFF(wk,0,getdate()), 0) union all
  
 -- First Day of the Year
-select DATEADD(yy, DATEDIFF(yy,0,getdate()), 0) 'First Day of the Year'
+select 'First Day of the Year' Description, DATEADD(yy, DATEDIFF(yy,0,getdate()), 0) union all
  
 -- First Day of the Quarter
-select DATEADD(qq, DATEDIFF(qq,0,getdate()), 0) 'First Day of the Quarter'
+select 'First Day of the Quarter' Description, DATEADD(qq, DATEDIFF(qq,0,getdate()), 0) union all
  
 -- Midnight for the Current Day
-select DATEADD(dd, DATEDIFF(dd,0,getdate()), 0) 'Midnight for the Current Day'
+select 'Midnight for the Current Day' Description, DATEADD(dd, DATEDIFF(dd,0,getdate()), 0) union all
  
 -- Last Day of Prior Month
-select dateadd(ms,-3,DATEADD(mm, DATEDIFF(mm,0,getdate()  ), 0)) 'Last Day of Prior Month'
+select 'Last Day of Prior Month' Description, dateadd(ms,-3,DATEADD(mm, DATEDIFF(mm,0,getdate()  ), 0)) union all
  
 -- Last Day of Prior Year
-select dateadd(ms,-3,DATEADD(yy, DATEDIFF(yy,0,getdate()  ), 0)) 'Last Day of Prior Year'
+select 'Last Day of Prior Year' Description, dateadd(ms,-3,DATEADD(yy, DATEDIFF(yy,0,getdate()  ), 0)) union all
  
 -- Last Day of Current Month
-select dateadd(ms,-3,DATEADD(mm, DATEDIFF(m,0,getdate()  )+1, 0)) 'Last Day of Current Month'
+select 'Last Day of Current Month' Description, dateadd(ms,-3,DATEADD(mm, DATEDIFF(m,0,getdate()  )+1, 0)) union all
  
 -- Last Day of Current Year
-select dateadd(ms,-3,DATEADD(yy, DATEDIFF(yy,0,getdate()  )+1, 0)) 'Last Day of Current Year'
+select 'Last Day of Current Year' Description, dateadd(ms,-3,DATEADD(yy, DATEDIFF(yy,0,getdate()  )+1, 0)) union all
  
 -- First Monday of the Month
-select DATEADD(wk, DATEDIFF(wk,0, dateadd(dd,6-datepart(day,getdate()),getdate()) ), 0)  'First Monday of the Month'
+select 'First Monday of the Month' Description, DATEADD(wk, DATEDIFF(wk,0, dateadd(dd,6-datepart(day,getdate()),getdate()) ), 0) 
+
+-- Sunday of the Current Week
+set DATEFIRST 1
+select 'Sunday of the Current Week' Description, DATEADD(dd, 1 - DATEPART(dw, getdate()), getdate())
+ 
