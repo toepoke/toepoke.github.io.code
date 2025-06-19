a950cb16ecac3b4e881e880279389d20369216bb
toepoke <956482+toepoke@users.noreply.github.com>
2025-06-19 13:50:30 +0100

    winget software installs for new pcs

---
+++ Cmd/new-pc-install.cmd
@@ -0,0 +1,28 @@
+
+winget install -e --id Microsoft.WindowsPCHealthCheck
+winget install -e --id OpenJS.NodeJS
+winget install -e --id Microsoft.NuGet
+winget install -e "Avast Free Antivirus"
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Google.Chrome
+winget install -e --id Google.Chrome.Canary
+winget install -e --id Notepad++.Notepad++
+winget install -e --id IDRIX.VeraCrypt
+winget install -e --id WinMerge.WinMerge
+
+:: Try separately
+	:: winget install -e --id Microsoft.SQLServer.2019.Developer --custom /SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS
+
+:: The following are portable installs:
+	:: KeePass is a portable install
+
+:: The following are manual
+	:: Might be better doing VS manually
+	:: Office do manually off network
+	:: winget install --id Microsoft.VisualStudio.2022.Community  -e
+	:: erazer
+	:: adobe cs4
+	:: IIS
+	:: IIS rewrite module????
+	:: https://www.7-zip.org/
+	:: Brother iPrint&Scan
