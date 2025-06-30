2b50fba2862fc1ff84130712ea093464ee4e89a9
fj <me@jarvis>
2025-06-30 14:24:53 +0100

    feat: More winget install items

--- Cmd/new-pc-install.cmd
+++ Cmd/new-pc-install.cmd
@@ -1,14 +1,28 @@
 
-winget install -e --id Microsoft.WindowsPCHealthCheck
+:: Development tools
 winget install -e --id OpenJS.NodeJS
 winget install -e --id Microsoft.NuGet
-winget install -e "Avast Free Antivirus"
 winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id WinMerge.WinMerge
+winget install -e --id Microsoft.SQLServerManagementStudio
+winget install -e --id Git.Git
+winget install -e --id Microsoft.PowerShell
+
+:: Utilities
+winget install -e --id Microsoft.WindowsPCHealthCheck
+winget install -e "Avast Free Antivirus"
 winget install -e --id Google.Chrome
 winget install -e --id Google.Chrome.Canary
 winget install -e --id Notepad++.Notepad++
 winget install -e --id IDRIX.VeraCrypt
-winget install -e --id WinMerge.WinMerge
+winget install -e --id Microsoft.PowerToys
+
+:: Productivity
+:: Outlook for Windows
+winget install -e --id=9NRX63209R7B -i --source=msstore --accept-package-agreements
+winget install -e --id=TheDocumentFoundation.LibreOffice
+
+
 
 :: Try separately
 	:: winget install -e --id Microsoft.SQLServer.2019.Developer --custom /SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS
