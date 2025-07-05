7c4dd1be4dfc5e9fc5041eb3d6e8105fe019cb0f
fj <956482+toepoke@users.noreply.github.com>
2025-07-05 11:28:41 +0100

    Update new-pc-install.cmd
    Added some more tools and stuff

--- Cmd/new-pc-install.cmd
+++ Cmd/new-pc-install.cmd
@@ -4,13 +4,13 @@ winget install -e --id OpenJS.NodeJS.TLS
 winget install -e --id Microsoft.NuGet
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Microsoft.VisualStudio.2022.Community
-winget install -e --id WinMerge.WinMerge
 winget install -e --id Microsoft.SQLServerManagementStudio
 winget install -e --id Git.Git
 winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.AzureCLI
 
 :: Utilities
+winget install -e --id WinMerge.WinMerge
 winget install -e --id Microsoft.WindowsPCHealthCheck
 winget install -e "Avast Free Antivirus"
 winget install -e --id Google.Chrome
@@ -18,6 +18,17 @@ winget install -e --id Google.Chrome.Canary
 winget install -e --id Notepad++.Notepad++
 winget install -e --id IDRIX.VeraCrypt
 winget install -e --id Microsoft.PowerToys
+winget install -e --id 7zip.7zip
+winget install -e --id PuTTY.PuTTY
+:: Mozilla Thunderbird
+winget install -e --id 9PM5VM1S3VMQ
+:: REST API tool (think Postman)
+winget install -e --id Insomnia.Insomnia
+:: Fiddler - take your pick
+winget install -e --id Telerik.Fiddler.Classic
+winget install -e --id Telerik.Fiddler.Everywhere
+winget install -e --id dotPDN.PaintDotNet
+
 
 :: Optional (uncomment to install)
 :: winget install -e --id  Microsoft.Azure.StorageExplorer
@@ -45,12 +56,8 @@ winget upgrade --all
 	:: KeePass is a portable install
 
 :: The following are manual
-	:: Might be better doing VS manually
-	:: Office do manually off network
-	:: winget install --id Microsoft.VisualStudio.2022.Community  -e
 	:: erazer
-	:: adobe cs4
 	:: IIS
-	:: IIS rewrite module????
+	:: IIS rewrite module
 	:: https://www.7-zip.org/
 	:: Brother iPrint&Scan
